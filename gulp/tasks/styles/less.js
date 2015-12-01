cf9bfd100d0833c07ab86f218195d6eb2b60b099
René Stalder <rene@whatwedo.ch>
2015-12-01 18:49:10 +0100

    refactor(styles): src as a array

--- gulp/tasks/styles/less.js
+++ gulp/tasks/styles/less.js
@@ -1,52 +1,34 @@
-'use strict';
+'use strict'
 
-var plumber       = require('gulp-plumber');
-var less          = require('gulp-less');
-var replace       = require('gulp-replace');
-var rename        = require('gulp-rename');
-var merge         = require('merge-stream');
-var browserSync   = require('browser-sync').create();
-var handleErrors  = require('../../util/handleErrors');
-var helper        = require('../../util/helpers')();
+var plumber = require('gulp-plumber')
+var less = require('gulp-less')
+var browserSync = require('browser-sync').create()
+var handleErrors = require('../../util/handleErrors')
+var helper = require('../../util/helpers')()
+var gutil = require('gulp-util')
 
-module.exports = function(gulp, config){
-  var src, dest, entries, options;
-  src = helper.getSrcPath(config, config.styles.options.less.src);
-  dest = config.options.tmpDir;
-  entries = Object.getOwnPropertyNames(src);
+module.exports = function (gulp, config) {
+  var src, dest, options
+  src = helper.getSrcPath(config, config.styles.options.less.src)
+  dest = config.dest
 
-  options = helper.copyLiteral(config.styles.options.less);
-  delete options.src;
+  options = helper.copyLiteral(config.styles.options.less)
+  delete options.src
 
-  gulp.task('styles-less', function() {
-    var gulpLessConfig = createConfig(config);
-    var tasks = entries.map(function(bundleName){
-      return gulp.src(src[bundleName])
-        .pipe(plumber())
-        .pipe(less(gulpLessConfig))
-        .pipe(rename(bundleName + '.less.css'))
-        .pipe(gulp.dest(dest))
-        .pipe(browserSync.stream())
-        // .pipe(autoprefixer(config.autoprefixer.options))                     // TODO: Add to style task
-        //.pipe(gulpif(argv.prod, minifycss(minifyOptions.prod)))
-        //.pipe(sourcemaps.init({loadMaps: true }))
-        //.pipe(sourcemaps.write('.', { includeConent: false,  sourceRoot: '.' }))
-        //.pipe(replace(/{PKG_VERSION}/g,  config.options.version))
-        //.pipe(gulp.dest(dest))
-        .on('error', handleErrors);
-    });
+  gutil.log(src)
 
-    return merge(tasks);
-  });
-
-  /**
-   * Creates a config for gulp-less out of craffft-config
-   * @param  {object} config craffft config
-   * @return {object}        gulp-less config
-   */
-  function createConfig(config){
-    return {
-      paths: [config.src]
-    };
-  }
-};
+  gulp.task('styles-less', function () {
+    return gulp.src(src, { base: config.src })
+      .pipe(plumber())
+      .pipe(less(options))
+      .pipe(gulp.dest(dest))
+      .pipe(browserSync.stream())
+      // .pipe(autoprefixer(config.autoprefixer.options))                     // TODO: Add to style task
+      // .pipe(gulpif(argv.prod, minifycss(minifyOptions.prod)))
+      // .pipe(sourcemaps.init({loadMaps: true }))
+      // .pipe(sourcemaps.write('.', { includeConent: false,  sourceRoot: '.' }))
+      // .pipe(replace(/{PKG_VERSION}/g,  config.options.version))
+      // .pipe(gulp.dest(dest))
+      .on('error', handleErrors)
+  })
+}
--- gulp/tasks/styles/stylus.js
+++ gulp/tasks/styles/stylus.js
@@ -1,40 +1,31 @@
-'use strict';
+'use strict'
 
-var plumber       = require('gulp-plumber');
-var stylus        = require('gulp-stylus');
-var replace       = require('gulp-replace');
-var rename        = require('gulp-rename');
-var merge         = require('merge-stream');
-var browserSync   = require('browser-sync').create();
-var handleErrors  = require('../../util/handleErrors');
-var helper        = require('../../util/helpers')();
+var plumber = require('gulp-plumber')
+var stylus = require('gulp-stylus')
+var browserSync = require('browser-sync').create()
+var handleErrors = require('../../util/handleErrors')
+var helper = require('../../util/helpers')()
 
-module.exports = function(gulp, config){
-  var src, dest, entries, options;
-  src = helper.getSrcPath(config, config.styles.options.stylus.src);
-  dest = config.options.tmpDir;
-  entries = Object.getOwnPropertyNames(src);
+module.exports = function (gulp, config) {
+  var src, dest, options
+  src = helper.getSrcPath(config, config.styles.options.stylus.src)
+  dest = config.dest
 
-  options = helper.copyLiteral(config.styles.options.stylus);
-  delete options.src;
+  options = helper.copyLiteral(config.styles.options.stylus)
+  delete options.src
 
-  gulp.task('styles-stylus', function() {
-    var tasks = entries.map(function(bundleName){
-      return gulp.src(src[bundleName])
-        .pipe(plumber())
-        .pipe(stylus(options))
-        .pipe(rename(bundleName + '.stylus.css'))
-        .pipe(gulp.dest(dest))
-        .pipe(browserSync.stream())
-        // .pipe(autoprefixer(config.autoprefixer.options))                     // TODO: Add to style task
-        //.pipe(gulpif(argv.prod, minifycss(minifyOptions.prod)))
-        //.pipe(sourcemaps.init({loadMaps: true }))
-        //.pipe(sourcemaps.write('.', { includeConent: false,  sourceRoot: '.' }))
-        //.pipe(replace(/{PKG_VERSION}/g,  config.options.version))
-        //.pipe(gulp.dest(dest))
-        .on('error', handleErrors);
-    });
-
-    return merge(tasks);
-  });
-};
+  gulp.task('styles-stylus', function () {
+    return gulp.src(src, { base: config.src })
+      .pipe(plumber())
+      .pipe(stylus(options))
+      .pipe(gulp.dest(dest))
+      .pipe(browserSync.stream())
+      // .pipe(autoprefixer(config.autoprefixer.options))                     // TODO: Add to style task
+      // .pipe(gulpif(argv.prod, minifycss(minifyOptions.prod)))
+      // .pipe(sourcemaps.init({loadMaps: true }))
+      // .pipe(sourcemaps.write('.', { includeConent: false,  sourceRoot: '.' }))
+      // .pipe(replace(/{PKG_VERSION}/g,  config.options.version))
+      // .pipe(gulp.dest(dest))
+      .on('error', handleErrors)
+  })
+}
