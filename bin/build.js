89806aad27486165ee3b101a7e199162c9e09dcc
René Stalder <rene@whatwedo.ch>
2015-12-30 20:21:40 +0100

    refactor(compile): run watcher after compile

--- bin/build.js
+++ bin/build.js
@@ -1,4 +1,4 @@
 #! /usr/bin/env node
 var shell = require('shelljs')
 
-shell.exec('node_modules/gulp/bin/gulp.js --silent --env production')
+shell.exec('node_modules/gulp/bin/gulp.js compile --silent --env production')
