b4d879064bfcf2d03d7aff0defbb6439b99b9ef5
René Stalder <rene@whatwedo.ch>
2016-06-06 16:40:56 +0200

    fix(errorHandler): remove system notification
    Remove system notifications via Growl and co. for compatibilty in Vagrant environment.

--- gulp/util/compileLogger.js
+++ gulp/util/compileLogger.js
@@ -3,27 +3,27 @@
  * https://github.com/vigetlabs/gulp-starter/
  */
 
-var gutil        = require('gulp-util');
-var prettifyTime = require('./prettifyTime');
-var handleErrors = require('./handleErrors');
+var gutil = require('gulp-util')
+var prettifyTime = require('./prettifyTime')
+var handleErrors = require('./handleErrors')
 
-module.exports = function(err, stats) {
-  'use strict';
+module.exports = function (err, stats) {
+  'use strict'
 
-  if(err) {
-    throw new gutil.PluginError('webpack', err);
+  if (err) {
+    throw new gutil.PluginError('webpack', err)
   }
 
-  var statColor = stats.compilation.warnings.length < 1 ? 'green' : 'yellow';
+  var statColor = stats.compilation.warnings.length < 1 ? 'green' : 'yellow'
 
-  if(stats.compilation.errors.length > 0) {
-    stats.compilation.errors.forEach(function(error){
-      handleErrors(error);
-      statColor = 'red';
-    });
+  if (stats.compilation.errors.length > 0) {
+    stats.compilation.errors.forEach(function (error) {
+      handleErrors(error)
+      statColor = 'red'
+    })
   } else {
-    var compileTime = prettifyTime(stats.endTime - stats.startTime);
-    gutil.log(gutil.colors[statColor](stats));
-    gutil.log('Compiled with', gutil.colors.cyan('webpack:development'), 'in', gutil.colors.magenta(compileTime));
+    var compileTime = prettifyTime(stats.endTime - stats.startTime)
+    gutil.log(gutil.colors[ statColor ](stats))
+    gutil.log('Compiled with craffft in', gutil.colors.magenta(compileTime))
   }
 };
